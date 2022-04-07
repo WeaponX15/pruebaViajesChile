28abfc23d1fcc99dfbba7910807c75d4af4f2967
WeaponX15 <juveftraru@gmail.com>
2022-04-07 12:59:30 -0400

    Commit 3 (modal, corrección miJS)

--- assets/js/miJS.js
+++ assets/js/miJS.js
@@ -13,8 +13,10 @@ $(function () {
         }
     });
 
-        //TOOLTIPS ICONOS "QUIÉNES SOMOS"
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
+    //TOOLTIPS ICONOS "QUIÉNES SOMOS"
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+})
