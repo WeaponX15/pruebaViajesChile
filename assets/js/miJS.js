c9210dbc61b4981917b9eb9a01202fb573d53041
WeaponX15 <juveftraru@gmail.com>
2022-04-07 13:56:33 -0400

    Commit 5 (ocultar párrafos, orden, popover, correcciones finales)

--- assets/js/miJS.js
+++ assets/js/miJS.js
@@ -13,10 +13,42 @@ $(function () {
         }
     });
 
+    //SMOOTH SCROLL DESTACADOS
+    $("#idDestacados").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    //SMOOTH SCROLL CONTACTO
+    $("#idFormulario").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
     //TOOLTIPS ICONOS "QUIÉNES SOMOS"
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    //POPOVERS IMÁGENES CARDS
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
     })
 
 })
